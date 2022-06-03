5ab23e11b6c85f18d537c1ab55faca842d4168de
Zhorobekov <zhorobekov25@icloud.com>
2022-06-03 22:49:15 +0600

    create searchBarController

--- iChat/Controllers/ListViewController.swift
+++ iChat/Controllers/ListViewController.swift
@@ -9,12 +9,60 @@ import UIKit
 
 class ListViewController: UIViewController {
     
+    var collectionView: UICollectionView!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .white
+        setupSearchBar()
+        setupCollectionView()
     }
     
+        private func setupSearchBar() {
+            navigationController?.navigationBar.barTintColor = .mainWhite()
+            navigationController?.navigationBar.shadowImage = UIImage()
+            
+            let searchController = UISearchController(searchResultsController: nil)
+            navigationItem.searchController = searchController
+            searchController.hidesNavigationBarDuringPresentation = false
+            searchController.obscuresBackgroundDuringPresentation = false
+            searchController.searchBar.delegate = self
+        }
+    
+    private func setupCollectionView() {
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UICollectionViewFlowLayout())
+        collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        collectionView.backgroundColor = .mainWhite()
+        view.addSubview(collectionView)
+        
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cellid")
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+    
+}
+
+//MARK: - SearchBarController
+extension ListViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        print(searchText)
+    }
+}
+
+// MARK: - UICollectionViewDelegate, UICollectionViewDataSource
+extension ListViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellid", for: indexPath)
+        cell.backgroundColor = .red
+        return cell
+    }
 }
 
 
@@ -30,10 +78,10 @@ struct ListViewControllerProvider: PreviewProvider {
     
     struct ContainerView: UIViewControllerRepresentable {
         
-        let listViewController = ListViewController()
+        let mainTabBarViewController = MainTabBarController()
         
         func makeUIViewController(context: Context) -> some UIViewController {
-            return listViewController
+            return mainTabBarViewController
         }
         
         func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
