f770c5655a50c44fbebddfa2566eadc577d5c8f4
Zhorobekov <zhorobekov25@icloud.com>
2022-06-05 15:31:01 +0600

    add collection view

--- iChat/Controllers/ListViewController.swift
+++ iChat/Controllers/ListViewController.swift
@@ -7,10 +7,38 @@
 
 import UIKit
 
+struct MChat: Hashable {
+    var userName: String
+    var userImage: UIImage
+    var lastMessage: String
+    var id = UUID()
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
+    
+    static func == (lhs: MChat, rhs: MChat) -> Bool {
+        return lhs.id == rhs.id
+    }
+}
+
 class ListViewController: UIViewController {
     
+    let activeChats: [MChat] = [
+        MChat(userName: "Alexey", userImage: UIImage(named: "human1")!, lastMessage: "How are you?"),
+        MChat(userName: "Bob", userImage: UIImage(named: "human2")!, lastMessage: "Hello!"),
+        MChat(userName: "Misha", userImage: UIImage(named: "human3")!, lastMessage: "Ok"),
+        MChat(userName: "Mila", userImage: UIImage(named: "human4")!, lastMessage: "See you soon")
+    
+    ]
+     
+    enum Section: Int, CaseIterable {
+        case activeChats
+    }
+    
+    var dataSource: UICollectionViewDiffableDataSource<Section, MChat>?
     var collectionView: UICollectionView!
-
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,29 +46,70 @@ class ListViewController: UIViewController {
         view.backgroundColor = .white
         setupSearchBar()
         setupCollectionView()
+        createDataSource()
+        reloadData()
     }
     
-        private func setupSearchBar() {
-            navigationController?.navigationBar.barTintColor = .mainWhite()
-            navigationController?.navigationBar.shadowImage = UIImage()
+    private func createDataSource() {
+        dataSource = UICollectionViewDiffableDataSource<Section, MChat> (collectionView: collectionView, cellProvider: { collectionView, indexPath, chat in
+            guard let section = Section(rawValue: indexPath.section) else { fatalError("Unknown section kind") }
             
-            let searchController = UISearchController(searchResultsController: nil)
-            navigationItem.searchController = searchController
-            searchController.hidesNavigationBarDuringPresentation = false
-            searchController.obscuresBackgroundDuringPresentation = false
-            searchController.searchBar.delegate = self
-        }
+            switch section {
+            case .activeChats:
+                let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellid", for: indexPath)
+                cell.backgroundColor = .systemBlue
+                return cell
+            }
+        })
+    }
+    
+    private func reloadData() {
+        var snapshot = NSDiffableDataSourceSnapshot<Section, MChat>()
+        snapshot.appendSections([.activeChats])
+        snapshot.appendItems(activeChats, toSection: .activeChats)
+        dataSource?.apply(snapshot, animatingDifferences: true)
+    }
+    
+    private func setupSearchBar() {
+        navigationController?.navigationBar.barTintColor = .mainWhite()
+        navigationController?.navigationBar.shadowImage = UIImage()
+        
+        let searchController = UISearchController(searchResultsController: nil)
+        navigationItem.searchController = searchController
+        searchController.hidesNavigationBarDuringPresentation = false
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.delegate = self
+    }
     
     private func setupCollectionView() {
-        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UICollectionViewFlowLayout())
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createCompositionLayout())
         collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         collectionView.backgroundColor = .mainWhite()
         view.addSubview(collectionView)
         
         collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cellid")
         
-        collectionView.delegate = self
-        collectionView.dataSource = self
+    }
+    
+    private func createCompositionLayout() -> UICollectionViewLayout {
+        
+        let layout = UICollectionViewCompositionalLayout { sectionIndex, layoutEnvironment in
+            
+            let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1),
+                                                  heightDimension: .fractionalHeight(1))
+            let item = NSCollectionLayoutItem(layoutSize: itemSize)
+            
+            let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1),
+                                                   heightDimension: .absolute(84))
+            let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitems: [item])
+            group.contentInsets = NSDirectionalEdgeInsets.init(top: 0, leading: 0, bottom: 8, trailing: 0)
+            
+            let section = NSCollectionLayoutSection(group: group)
+            section.contentInsets = NSDirectionalEdgeInsets.init(top: 16, leading: 20, bottom: 0, trailing: 20)
+            
+            return section
+        }
+        return layout
     }
     
 }
@@ -52,18 +121,6 @@ extension ListViewController: UISearchBarDelegate {
     }
 }
 
-// MARK: - UICollectionViewDelegate, UICollectionViewDataSource
-extension ListViewController: UICollectionViewDelegate, UICollectionViewDataSource {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 5
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellid", for: indexPath)
-        cell.backgroundColor = .red
-        return cell
-    }
-}
 
 
 //MARK: SwiftUI
@@ -78,7 +135,7 @@ struct ListViewControllerProvider: PreviewProvider {
     
     struct ContainerView: UIViewControllerRepresentable {
         
-        let mainTabBarViewController = MainTabBarController()
+        let mainTabBarViewController = ListViewController()
         
         func makeUIViewController(context: Context) -> some UIViewController {
             return mainTabBarViewController
--- iChat/Controllers/PeopleViewController.swift
+++ iChat/Controllers/PeopleViewController.swift
@@ -13,5 +13,51 @@ class PeopleViewController: UIViewController {
         super.viewDidLoad()
         
         view.backgroundColor = .white
+        setupSearchBar()
+    }
+    
+    private func setupSearchBar() {
+        navigationController?.navigationBar.barTintColor = .mainWhite()
+        navigationController?.navigationBar.shadowImage = UIImage()
+        
+        let searchController = UISearchController(searchResultsController: nil)
+        navigationItem.searchController = searchController
+        searchController.hidesNavigationBarDuringPresentation = false
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.delegate = self
     }
 }
+
+//MARK: - SearchBarController
+extension PeopleViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        print(searchText)
+    }
+}
+
+
+//MARK: SwiftUI
+import SwiftUI
+
+struct PeopleViewControllerProvider: PreviewProvider {
+    
+    static var previews: some View {
+        ContainerView()
+            .edgesIgnoringSafeArea(.all)
+    }
+    
+    struct ContainerView: UIViewControllerRepresentable {
+        
+        let mainTabBarViewController = MainTabBarController()
+        
+        func makeUIViewController(context: Context) -> some UIViewController {
+            return mainTabBarViewController
+        }
+        
+        func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+            
+        }
+    }
+}
+
+
