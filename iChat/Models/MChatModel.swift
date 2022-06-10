2a1287dd59062f152aa3122ad2986a238c060a46
Zhorobekov <zhorobekov25@icloud.com>
2022-06-10 23:57:43 +0600

    create Chat model and folder

---
+++ iChat/Models/MChatModel.swift
@@ -0,0 +1,23 @@
+//
+//  MChatModel.swift
+//  iChat
+//
+//  Created by Эрмек Жоробеков on 10.06.2022.
+//
+
+import Foundation
+
+struct MChat: Hashable, Decodable {
+    var username: String
+    var userImageString: String
+    var lastMessage: String
+    var id: Int
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
+    
+    static func == (lhs: MChat, rhs: MChat) -> Bool {
+        return lhs.id == rhs.id
+    }
+}
